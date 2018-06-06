2c159f50ed9f99d5508f1e344e3277f01290a568
Lee Spottiswood <me@lee.io>
2018-06-06 22:55:40 +0100

    Create img.lua

---
+++ img.lua
@@ -0,0 +1,33 @@
+-- I apologise profoundly for the following
+
+hexchat.register("img.lee.io plugin", "0.1", "An horrendous plugin to upload images from the clipboard to img.lee.io")
+
+local function img(data)
+    -- if in doubt, shell out
+    local p = io.popen("ImageUploader.exe")
+    local std_out = p:read('*l')
+    p:close()
+
+    if (std_out == nil) then
+        hexchat.print("Unexpected nil response from ImageUploader - check path")
+        return
+    end
+
+    -- this is just terrible - cannot access exitcode with io.popen (and only exitcode with os.execute), so lets check by just regexing out a URL instead
+    if (std_out:match("^(http)")) then
+        hexchat.command("say " .. std_out)
+    else
+        hexchat.print("Error: " .. std_out)
+    end
+
+    return hexchat.EAT_HEXCHAT
+end
+
+local function unload()
+    hexchat.print("Unloading img.lee.io plugin")
+end
+
+hexchat.hook_command("img", img)
+hexchat.hook_unload(unload)
+
+hexchat.print("Loaded img.lee.io plugin")
